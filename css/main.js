755f1a92fd76a388a9d666e9c326f8c986d2a381
vaishfxmedia <techthatmatter845@gmail.com>
2025-10-18 13:09:33 +0530

    Add files via upload

---
+++ css/main.js
@@ -0,0 +1,37 @@
+
+
+
+    // if reels aren't all uploaded
+
+    let reelTrack = document.getElementsByClassName('reel-track');
+    let reelsArray = document.getElementsByClassName('reel')
+
+console.log(reelTrack);
+
+ console.log("value of SRC in video tag :- ", reelsArray[9].childNodes[0].src == "file:///C:/Temporary/WEB%20TECH%20Workspace/vaishfxmedia/main.html#"
+);
+ console.log(reelsArray.length);
+ console.log(reelsArray);
+ 
+let video = 0;
+
+
+for( let i = reelsArray.length - 1 ;i>= 0;i--){
+
+     video = reelsArray[i].querySelector('video'); // get the video inside the reel
+    
+   
+    
+    
+    if (!video || !video.src || video.src.trim() === "" || video.src.endsWith("#")) {
+     
+        reelsArray[i].remove(); // remove the reel div
+
+    }
+     
+       
+
+
+
+}
+
